9956bb86d20e0f5130fde3873f78307b64f4e719
easyone <thinkanddoit@naver.com>
2022-12-20 17:08:55 +0900

    feat: 블로그 글 maxWidth 수정
    24 -> 30

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,7 +16,7 @@ export default ({ data }) => {
       style={{
         marginLeft: `auto`,
         marginRight: `auto`,
-        maxWidth: rhythm(24),
+        maxWidth: rhythm(30),
         padding: `${rhythm(0.5)} ${rhythm(3 / 4)} ${rhythm(1.5)} ${rhythm(
           3 / 4
         )}`,
